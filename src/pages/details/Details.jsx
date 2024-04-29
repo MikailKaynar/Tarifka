ca9c66f2286a83dda5ac2868fa9c0583aecae60d
Mikail kaynar <75529228+MikailKaynar@users.noreply.github.com>
2024-04-29 17:05:18 +0300

    Main
    Patika "React Native" kursu, "Tarifka" ödevi

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,45 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Config from 'react-native-config';
+import useFetch from '../../hooks/useFetch';
+import Loading from '../../components/loading';
+import DetailItem from '../../components/DetailItem';
+import {FlatList} from 'react-native-gesture-handler';
+
+const Details = ({route}) => {
+  const {idMeal} = route.params;
+  const url = Config.API_URL + 'lookup.php?i=' + idMeal;
+  const {loading, data, error} = useFetch(url);
+
+  if (error) {
+    return (
+      <View style={{flex: 1}}>
+        <Text>{error}</Text>
+        <Error />
+      </View>
+    );
+  }
+  if (loading) {
+    return <Loading />;
+  }
+
+  const renderProducts = ({item}) => <DetailItem item={item} />;
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={data.meals}
+        renderItem={renderProducts}
+        keyExtractor={item => item.idMeal}
+      />
+    </View>
+  );
+};
+
+export default Details;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
